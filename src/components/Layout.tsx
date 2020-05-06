88e6d41ab21d35628ce9dd654a5756562858f678
ninja-2020 <chengcheng@onescorpion.com>
2020-05-06 19:12:59 +0800

    daily update

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,44 @@
 import * as React from 'react';
+// import styled from 'styled-components';
+
+// import {
+//   // Drawer,
+//   // List,
+//   // ListItem,
+//   // ListItemIcon,
+//   // ListItemText,
+//   // AppBar,
+//   // IconButton,
+//   makeStyles,
+//   Theme,
+//   createStyles,
+// } from '@material-ui/core';
+
+// const drawerWidth = 240;
+// const useStyles = makeStyles((theme: Theme) =>
+//   createStyles({
+//     toolbar: theme.mixins.toolbar,
+//     drawer: {
+//       [theme.breakpoints.up('sm')]: {
+//         width: drawerWidth,
+//         flexShrink: 0,
+//       },
+//     },
+//     appBar: {
+//       [theme.breakpoints.up('sm')]: { display: 'none' },
+//       marginLeft: drawerWidth,
+//     },
+//   })
+// );
 
 export default function Layout() {
-  return <div></div>;
+  // const classes = useStyles();
+
+  // const SideBar = (
+  //   <div>
+  //     <div className={classes.toolbar}></div>
+  //   </div>
+  // );
+
+  return <div>{/* <SideBar /> */}</div>;
 }
